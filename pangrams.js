b777c278b75f03827f6bb50efe16eb2cbff2684c
kenpeter <kenpeter@kenpeter>
2018-08-20 13:09:14 +0000

    up

---
+++ pangrams.js
@@ -0,0 +1,11 @@
+
+function pangrams(s){      
+   let condi = /(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z)./i.test(s);
+
+    if(condi)
+        return 'pangram';
+    else
+        return 'not pangram';
+}
+
+
